27e8b30435c106acec88ff9a6261625666e2ca6f
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-14 19:26:52 +0200

    added first component structure to the project

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template >
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:'AppFooter',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="scss">
+    @use '../styles/partials/mixins' as*;
+    @use '../styles/partials/variables' as*;
+</style>
