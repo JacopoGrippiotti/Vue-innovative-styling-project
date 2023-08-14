9a40ab23ea92c99ef8593fdb90dfd54a3be59931
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-14 19:19:04 +0200

    added AppHEader component and deleted HelloWorld component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template >
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppHeader',
+   
+  data() {
+    return {
+        
+    }
+  },    
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
