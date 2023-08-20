9e3a64ede7c3d391a3f674ca8a11766a5c55d7f2
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-20 19:10:07 +0200

    added emit and appheader component to appmain component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,31 +1,38 @@
 <template>
-    ciao
+    <AppHeader @changedValue="apiCall"/>
+    <div>
+        {{ response }}
+    </div>
 </template>
 <script>
 import axios from 'axios';
+import AppHeader from './AppHeader.vue';
 export default {
     name:'AppMain',
+    components:{
+        AppHeader
+    },
     data() {
         return {
             apiUrl:'https://flynn.boolean.careers/exercises/api/random/word',
-            response:[],
+            response:"",
         }
     },
     methods: {
-        apiCall(){
+        apiCall(numberOfWords){
+            for(let i = 0; i<numberOfWords; i++){
             axios.get(this.apiUrl).then(
                 (response) => {
-                    this.response = response.data.response
+                    this.response += response.data.response
                     console.log(this.response)
                 }
-            )
-
-            
+            )    
         }
     },
     mounted(){
         this.apiCall()
     },
+ }
 }
 </script>
 <style lang="scss" scoped>
